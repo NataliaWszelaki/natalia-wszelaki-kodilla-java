f0f2ed7fd6e6b4f4a99ee6c71e35bcf869d32288
Natalia Wszelaki <natalia.slocinska@gmail.com>
2023-03-08 22:10:11 +0100

    Test - pierwszy test jednostkowy

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,38 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
-    public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main (String[]args){
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+        Calculator calculator = new Calculator(12,19);
+
+        int resultSubtract = calculator.subtractBFromA();
+        System.out.println("Subtract b from a = " + resultSubtract);
+        if (resultSubtract == 7) {
+            System.out.println("Correct result!");
+        } else {
+            System.out.println("Incorrect result!");
+        }
+
+        int resultAdd = calculator.addAToB();
+        System.out.println("Add a to b = " + resultAdd);
+        if (resultAdd == 31) {
+            System.out.println("Correct result!");
+        } else {
+            System.out.println("Incorrect result!");
+        }
+
     }
 }
